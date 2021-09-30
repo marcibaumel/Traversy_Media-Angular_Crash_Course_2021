423900d4ca7f30ebc8c0a33f25e2a7b61803f8ae
Baumel Márton <marcibaumel@gmail.com>
2021-09-30 18:35:34 +0200

    task service

--- angular-crash/src/app/components/tasks/tasks.component.ts
+++ angular-crash/src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Task} from '../../Task';
-import {TASKS} from '../../mock-tasks';
+import { TaskService } from 'src/app/services/task.service';
+
 
 @Component({
   selector: 'app-tasks',
@@ -9,10 +10,13 @@ import {TASKS} from '../../mock-tasks';
 })
 export class TasksComponent implements OnInit {
 
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
 
-  constructor() { }
 
-  ngOnInit(): void {}
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) =>(this.tasks = tasks));
+  }
 
 }
